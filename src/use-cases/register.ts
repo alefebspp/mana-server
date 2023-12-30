bd4ef9ef32ad8ec9a8717ea94c3fd5f5cf7b6f19
Alefe Bispo <alefe.bispo@onesight.com.br>
2023-12-30 16:51:23 -0300

    register use case tests

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,6 @@
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
 import { UsersRepository } from '@/repositories/users-repository'
+import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest{
@@ -8,12 +9,16 @@ interface RegisterUseCaseRequest{
   password: string
 }
 
+interface RegisterUseCaseResponse {
+  user: User
+}
+
 export class RegisterUseCase {
 
   constructor(private usersRepository: UsersRepository) {
   }
 
-  async execute({name, email, password}: RegisterUseCaseRequest) {
+  async execute({name, email, password}: RegisterUseCaseRequest): Promise<RegisterUseCaseResponse> {
     const password_hash = await hash(password, 6)
   
     const userWithSameEmail = await this.usersRepository.findByEmail(email)
@@ -23,11 +28,15 @@ export class RegisterUseCase {
     }
   
   
-    await this.usersRepository.create({
+    const user = await this.usersRepository.create({
       name,
       email,
       password_hash
     })
+
+    return {
+      user
+    }
   }
 }
 
