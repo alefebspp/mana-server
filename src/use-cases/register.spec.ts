2a239139b76a6e2fc99d03e0929150d1bca2d529
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-01-02 22:26:25 -0300

    make sut before each test

--- src/use-cases/register.spec.ts
+++ src/use-cases/register.spec.ts
@@ -1,16 +1,22 @@
-import {expect, it, describe} from 'vitest'
+import {expect, it, describe, beforeEach} from 'vitest'
 import { InMemoryUsersRepository } from '@/repositories/in-memory/in-memory-users-repository'
 import { RegisterUseCase } from './register'
 import { compare } from 'bcryptjs'
 import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
+import { UsersRepository } from '@/repositories/users-repository'
 
+let usersRepository: UsersRepository
+let sut: RegisterUseCase
 
 describe('Register Use Case', () => {
+
+  beforeEach(() => {
+    usersRepository = new InMemoryUsersRepository()
+    sut = new RegisterUseCase(usersRepository)
+  })
+
   it('should be able to register', async() => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-    
-    const {user} = await registerUseCase.execute({
+    const {user} = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456'
@@ -20,10 +26,7 @@ describe('Register Use Case', () => {
   })
 
   it('should hash user password upon registration', async() => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-    
-    const {user} = await registerUseCase.execute({
+    const {user} = await sut.execute({
       name: 'John Doe',
       email: 'johndoe@example.com',
       password: '123456'
@@ -34,19 +37,16 @@ describe('Register Use Case', () => {
     expect(isPasswordCorrectlyHashed).toBe(true)
   })
 
-  it('should not be able to register with same email twice', async() => {
-    const usersRepository = new InMemoryUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
-    
+  it('should not be able to register with same email twice', async() => {   
     const email = 'johndoe@example.com'
 
-    await registerUseCase.execute({
+    await sut.execute({
       name: 'John Doe',
       email,
       password: '123456'
     })
 
-    expect(() => registerUseCase.execute({
+    expect(() => sut.execute({
       name: 'John Doe',
       email,
       password: '123456'
