7fd3a5d443b699e22edd5d972dcfaa9cd0e91ba1
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-01-02 22:27:48 -0300

    create authenticate use case and route

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,3 +1,4 @@
+import { authenticate } from './controllers/authenticate'
 import { createCategory } from './controllers/categories/create'
 import { listCategories } from './controllers/categories/list'
 import { register } from './controllers/register'
@@ -5,6 +6,7 @@ import { FastifyInstance } from 'fastify'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
+  app.post('/sessions', authenticate)
   app.post('/categories', createCategory)
   app.get('/categories', listCategories)
 }
