9be777a97fc0a35d58cbe0236c2825ac2950da50
Alefe Bispo <106111153+alefebspp@users.noreply.github.com>
2024-01-13 16:18:23 -0300

    Revert "Feature/refactors"

--- src/use-cases/factories/categories/make-find-category-use-case.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaCategoriesRepository } from '@/repositories/prisma/prisma-categories-repository'
-import { FindCategoryUseCase } from '@/use-cases/find-category-use-case'
-
-export function makeFindCategoryUseCase() {
-  const categoriesRepository = new PrismaCategoriesRepository()
-  const findCategoryUseCase = new FindCategoryUseCase(categoriesRepository)
-  return findCategoryUseCase
-}
--- src/use-cases/factories/categories/make-update-category-use-case.ts
+++
@@ -1,8 +0,0 @@
-import { PrismaCategoriesRepository } from '@/repositories/prisma/prisma-categories-repository'
-import { UpdateCategoryUseCase } from '@/use-cases/update-category-use-case'
-
-export function makeUpdateCategoryUseCase() {
-  const categoriesRepository = new PrismaCategoriesRepository()
-  const updateCategoryUseCase = new UpdateCategoryUseCase(categoriesRepository)
-  return updateCategoryUseCase
-}
