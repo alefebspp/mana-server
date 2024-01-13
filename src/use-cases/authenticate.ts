40d2c633df84b01e198276273915f437f6d241b7
Alefe Bispo <alefe.bispo@onesight.com.br>
2024-01-13 12:58:30 -0300

    move errors to use cases folder

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -1,5 +1,5 @@
-import { InvalidCredentialsError } from '@/errors/invalid-credentials-error'
 import { UsersRepository } from '@/repositories/users-repository'
+import { InvalidCredentialsError } from './errors/invalid-credentials-error'
 import { User } from '@prisma/client'
 import { compare } from 'bcryptjs'
 
--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,5 @@
-import { UserAlreadyExistsError } from '@/errors/user-already-exists-error'
 import { UsersRepository } from '@/repositories/users-repository'
+import { UserAlreadyExistsError } from './errors/user-already-exists-error'
 import { User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 
